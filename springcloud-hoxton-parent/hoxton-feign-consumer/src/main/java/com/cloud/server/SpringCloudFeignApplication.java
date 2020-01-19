15ffeee8ef3bf632a710bef55bdcdce3d398115f
carxue <xuekui_it@163.com>
2020-01-19 18:05:53 +0800

    feign整合dashboard

--- springcloud-hoxton-parent/hoxton-feign-consumer/src/main/java/com/cloud/server/SpringCloudFeignApplication.java
+++ springcloud-hoxton-parent/hoxton-feign-consumer/src/main/java/com/cloud/server/SpringCloudFeignApplication.java
@@ -2,15 +2,35 @@ package com.cloud.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients(basePackages = {"com.cloud.service"})//如果不指定接口路径将无法实现依赖实例化
 @ComponentScan(basePackages = { "com.cloud.*" })
 public class SpringCloudFeignApplication {
+	
+	@Bean
+	public ServletRegistrationBean<HystrixMetricsStreamServlet> getServlet() {
+	        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+	        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<>(streamServlet);
+	        registrationBean.setLoadOnStartup(1);
+	        registrationBean.addUrlMappings("/hystrix.stream");
+	        registrationBean.setName("HystrixMetricsStreamServlet");
+	        return registrationBean;
+	    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudFeignApplication.class, args);
