ae443a2f63f0ea199f2fffe90b332a8b293cf902
carxue <xuekui_it@163.com>
2020-01-20 19:30:01 +0800

    提交

--- springcloud-hoxton-parent/hoxton-turbine-amqp/src/main/java/com/cloud/server/SpringCloudTurbineAmqpApplication.java
+++ springcloud-hoxton-parent/hoxton-turbine-amqp/src/main/java/com/cloud/server/SpringCloudTurbineAmqpApplication.java
@@ -2,21 +2,14 @@ package com.cloud.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
-import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
-
-@EnableHystrix
-@EnableCircuitBreaker
 /*Dashboard监控*/
 @EnableHystrixDashboard
 @EnableTurbineStream
@@ -31,16 +24,6 @@ public class SpringCloudTurbineAmqpApplication {
 		return new RestTemplate();
 	}
 	
-	@Bean
-	public ServletRegistrationBean<HystrixMetricsStreamServlet> getServlet() {
-	        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
-	        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<>(streamServlet);
-	        registrationBean.setLoadOnStartup(1);
-	        registrationBean.addUrlMappings("/hystrix.stream");
-	        registrationBean.setName("HystrixMetricsStreamServlet");
-	        return registrationBean;
-	}   
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudTurbineAmqpApplication.class, args);
 	}
