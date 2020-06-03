178dc6326516840302f6a13878d0be487d9882ab
carlxue <xuekui_it@163.com>
2020-06-03 15:34:36 +0800

    nacos服务体积

---
+++ springcloud-hoxton-parent/nacos-provider-peer2/src/main/java/com/cloud/server/NacoProvider2Application.java
@@ -0,0 +1,17 @@
+package com.cloud.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@ComponentScan(basePackages = { "com.cloud.*" })
+public class NacoProvider2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NacoProvider2Application.class, args);
+	}
+}
