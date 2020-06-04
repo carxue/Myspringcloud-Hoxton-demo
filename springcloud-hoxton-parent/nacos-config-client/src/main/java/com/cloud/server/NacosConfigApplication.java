2eaacbd524427fcf871f70703374dfffdb112946
carlxue <xuekui_it@163.com>
2020-06-04 11:16:16 +0800

    nacos configdemo体积

---
+++ springcloud-hoxton-parent/nacos-config-client/src/main/java/com/cloud/server/NacosConfigApplication.java
@@ -0,0 +1,16 @@
+package com.cloud.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@ComponentScan(basePackages = { "com.cloud.*" })
+public class NacosConfigApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(NacosConfigApplication.class, args);
+	}
+}
